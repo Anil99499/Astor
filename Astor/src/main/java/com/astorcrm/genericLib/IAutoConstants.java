a147f572153432719016f9f100dc993893269219
Anil <Anil@DESKTOP-7BFRNTN>
2021-11-20 03:09:38 +0530

    c

---
+++ Astor/src/main/java/com/astorcrm/genericLib/IAutoConstants.java
@@ -0,0 +1,13 @@
+package com.astorcrm.genericLib;
+
+public interface IAutoConstants {
+
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="F:\\Mavenproject\\Astor\\src\\test\\resources\\chromedriver.exe";
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="F:\\Mavenproject\\Astor\\src\\test\\resources\\geckodriver.exe";
+	
+	String PROP_PATH="F:\\Mavenproject\\Astor\\src\\main\\resources\\config.properties";
+	
+}
